c476df68a5242276f2864f4b860673edfbbb2335
rodmar2 <gerocoma-public@yahoo.com>
2014-11-25 12:08:42 -0600

    Auto ID is working !

--- Invoices_v1/ViewController/src/com/globant/invoices/view/backing/page/RegisterInvoice_v2.java
+++ Invoices_v1/ViewController/src/com/globant/invoices/view/backing/page/RegisterInvoice_v2.java
@@ -1,11 +1,44 @@
 package com.globant.invoices.view.backing.page;
 
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.binding.OperationBinding;
+
 import oracle.adf.view.rich.component.rich.RichDocument;
 import oracle.adf.view.rich.component.rich.RichForm;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.component.rich.input.RichInputDate;
+import oracle.adf.view.rich.component.rich.input.RichInputText;
+import oracle.adf.view.rich.component.rich.layout.RichPanelFormLayout;
+import oracle.adf.view.rich.component.rich.layout.RichPanelGroupLayout;
+import oracle.adf.view.rich.component.rich.nav.RichCommandButton;
+import oracle.adf.view.rich.component.rich.output.RichMessages;
+import oracle.adf.view.rich.component.rich.output.RichOutputText;
+
+import org.apache.myfaces.trinidad.component.UIXGroup;
 
 public class RegisterInvoice_v2 {
     private RichForm f1;
     private RichDocument d1;
+    private RichPanelGroupLayout pgl1;
+    private RichOutputText ot1;
+    private RichMessages m1;
+    private RichPanelFormLayout pfl1;
+    private RichInputDate id1;
+    private RichInputText it2;
+    private RichCommandButton cb1;
+    private UIXGroup g1;
+    private RichCommandButton cb2;
+    private RichPanelGroupLayout pgl2;
+    private RichCommandButton cb4;
+    private RichInputText it1;
+    private RichCommandButton cb5;
+    private RichPanelFormLayout pfl2;
+    private RichInputDate id2;
+    private RichInputText it3;
+    private RichInputText it4;
+    private RichCommandButton cb6;
+    private RichTable t1;
 
     public void setF1(RichForm f1) {
         this.f1 = f1;
@@ -21,5 +54,202 @@ public class RegisterInvoice_v2 {
 
     public RichDocument getD1() {
         return d1;
+    }
+
+    public void setPgl1(RichPanelGroupLayout pgl1) {
+        this.pgl1 = pgl1;
+    }
+
+    public RichPanelGroupLayout getPgl1() {
+        return pgl1;
+    }
+
+    public void setOt1(RichOutputText ot1) {
+        this.ot1 = ot1;
+    }
+
+    public RichOutputText getOt1() {
+        return ot1;
+    }
+
+    public void setM1(RichMessages m1) {
+        this.m1 = m1;
+    }
+
+    public RichMessages getM1() {
+        return m1;
+    }
+
+    public void setPfl1(RichPanelFormLayout pfl1) {
+        this.pfl1 = pfl1;
+    }
+
+    public RichPanelFormLayout getPfl1() {
+        return pfl1;
+    }
+
+
+    public void setId1(RichInputDate id1) {
+        this.id1 = id1;
+    }
+
+    public RichInputDate getId1() {
+        return id1;
+    }
+
+    public void setIt2(RichInputText it2) {
+        this.it2 = it2;
+    }
+
+    public RichInputText getIt2() {
+        return it2;
+    }
+
+
+    public void setCb1(RichCommandButton cb1) {
+        this.cb1 = cb1;
+    }
+
+    public RichCommandButton getCb1() {
+        return cb1;
+    }
+    
+    /**
+     * To test usage in buttons.
+     */
+    public void sayHello() {
+        System.out.println(String.format("Hello people !"));
+    }
+    
+    /**
+     * Starting transaction for adding new invoice to database.
+     */
+    public void createInvoice() {
+        System.out.println(String.format("Trying to insert new invoice..."));
+        
+        DCBindingContainer bindings = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();        
+        System.out.println(String.format("bindings: %s", bindings));
+        
+        OperationBinding operation = bindings.getOperationBinding("CreateInsert");
+        System.out.println(String.format("operation: %s", operation));
+        
+        Object o = operation.execute();
+        System.out.println(String.format("execution: %s", o));
+    }
+    
+    /**
+     * Saving into DB.
+     */
+    public void saveInvoice() {
+        System.out.println(String.format("Trying to saveInvoice..."));
+        
+        DCBindingContainer bindings = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();                
+        System.out.println(String.format("bindings: %s", bindings));
+        
+        OperationBinding operation = bindings.getOperationBinding("Commit");
+        System.out.println(String.format("operation: %s", operation));
+        
+        Object o = operation.execute();
+        System.out.println(String.format("execution: %s", o));
+    }
+
+    public void setG1(UIXGroup g1) {
+        this.g1 = g1;
+    }
+
+    public UIXGroup getG1() {
+        return g1;
+    }
+
+    public void setCb2(RichCommandButton cb2) {
+        this.cb2 = cb2;
+    }
+
+    public RichCommandButton getCb2() {
+        return cb2;
+    }
+
+    public void setPgl2(RichPanelGroupLayout pgl2) {
+        this.pgl2 = pgl2;
+    }
+
+    public RichPanelGroupLayout getPgl2() {
+        return pgl2;
+    }
+
+
+    public void setCb4(RichCommandButton cb4) {
+        this.cb4 = cb4;
+    }
+
+    public RichCommandButton getCb4() {
+        return cb4;
+    }
+
+    public void setIt1(RichInputText it1) {
+        this.it1 = it1;
+    }
+
+    public RichInputText getIt1() {
+        return it1;
+    }
+
+
+    public void setCb5(RichCommandButton cb5) {
+        this.cb5 = cb5;
+    }
+
+    public RichCommandButton getCb5() {
+        return cb5;
+    }
+
+
+    public void setPfl2(RichPanelFormLayout pfl2) {
+        this.pfl2 = pfl2;
+    }
+
+    public RichPanelFormLayout getPfl2() {
+        return pfl2;
+    }
+
+    public void setId2(RichInputDate id2) {
+        this.id2 = id2;
+    }
+
+    public RichInputDate getId2() {
+        return id2;
+    }
+
+    public void setIt3(RichInputText it3) {
+        this.it3 = it3;
+    }
+
+    public RichInputText getIt3() {
+        return it3;
+    }
+
+    public void setIt4(RichInputText it4) {
+        this.it4 = it4;
+    }
+
+    public RichInputText getIt4() {
+        return it4;
+    }
+
+    public void setCb6(RichCommandButton cb6) {
+        this.cb6 = cb6;
+    }
+
+    public RichCommandButton getCb6() {
+        return cb6;
+    }
+
+
+    public void setT1(RichTable t1) {
+        this.t1 = t1;
+    }
+
+    public RichTable getT1() {
+        return t1;
     }
 }
